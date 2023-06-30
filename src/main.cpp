5a60e8ebf24cfa47fc517cd38c5ce97725504a8a
Nikolas Koesling <nikolas@koesling.info>
2023-06-30 10:00:08 +0200

    configurable cycle time and cycle time monitoring

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,12 @@
 #include "license.hpp"
 
 #include <array>
+#include <chrono>
 #include <csignal>
 #include <filesystem>
 #include <iostream>
 #include <sysexits.h>
+#include <thread>
 
 // cxxopts, but all warnings disabled
 #ifdef COMPILER_CLANG
@@ -78,6 +80,11 @@ int main(int argc, char **argv) {
                           "to exist as an orphan and is no longer used.");
     options.add_options()("q,quiet", "Disable output");
     options.add_options()("d,debug", "Enable modbus debug output");
+    options.add_options()("c,cycle",
+                          "set cycle time in ms (default: 0; as fast as possible)",
+                          cxxopts::value<std::size_t>()->default_value("0"));
+    options.add_options()("no-cycle-time-fail", "Do not fail if the cycle time is repeatedly exceeded");
+    options.add_options()("no-cycle-time-warn", "Do not print a warning if the cycle time is exceeded");
     options.add_options()("read-start-image",
                           "do not initialize output registers with zero, but read values from coupler");
     options.add_options()(
@@ -125,21 +132,25 @@ int main(int argc, char **argv) {
     }
 
     if (args.count("host") == 0) {
-        std::cerr << "ERROR: no host specified" << std::endl;
+        std::cerr << Print_Time::iso << " ERROR: no host specified" << std::endl;
         return exit_usage();
     }
 
-    const std::string &service     = args.count("service") ? args["service"].as<std::string>() : "502";
-    const auto         FORCE_SHM   = args.count("force") > 0;
-    const auto         QUIET       = args.count("quiet") > 0;
-    const auto         START_IMAGE = args.count("read-start-image") > 0;
+    const std::string &service      = args.count("service") ? args["service"].as<std::string>() : "502";
+    const auto         FORCE_SHM    = args.count("force") > 0;
+    const auto         QUIET        = args.count("quiet") > 0;
+    const auto         START_IMAGE  = args.count("read-start-image") > 0;
+    const auto         CYCLE_TIME   = args["cycle"].as<std::size_t>();
+    const auto         CYCLE_NOFAIL = args.count("no-cycle-time-fail");
+    const auto         CYCLE_NOWARN = args.count("no-cycle-time-warn");
 
     WAGO_Modbus::TCP_Coupler_SHM wago(args["host"].as<std::string>(), service, args.count("debug") > 0 && !QUIET);
 
     try {
         wago.init(args["prefix"].as<std::string>(), !FORCE_SHM);
     } catch (const std::exception &e) {
-        std::cerr << "Failed to connect to WAGO Field bus coupler: " << e.what() << std::endl;
+        std::cerr << Print_Time::iso << " ERROR: Failed to connect to WAGO Field bus coupler: " << e.what()
+                  << std::endl;
         return EX_UNAVAILABLE;
     }
 
@@ -159,20 +170,34 @@ int main(int argc, char **argv) {
         try {
             wago.fetch_image(true);
         } catch (const std::exception &e) {
-            std::cerr << "Failed to fetch start image: " << e.what() << std::endl;
-            std::cerr << "Terminating..." << std::endl;
+            std::cerr << Print_Time::iso << " ERROR: Failed to fetch start image: " << e.what() << std::endl;
             return EX_SOFTWARE;
         }
     }
 
     int ret = EX_OK;
 
-    // TODO cycle time
+    /*
+     * Cycle time Failure mechanism:
+     *
+     * If the cycle time is exceeded, the cycle_fail counter is incremented by 10.
+     * If the cycle time is not exceeded AND the cycle_fail counter is >0, cycle_fail is decremented by 1.
+     * The application is terminated if cycle_fail is greather than MAX_FAIL.
+     *
+     * This mechanism quickly terminates the program if the cycle time is permanently exceeded,
+     * but sporadic exceeding does not lead to termination
+     */
+    const std::size_t MAX_FAIL   = 100;
+    std::size_t       cycle_fail = 0;
+
+    // time until the thread will sleep to wait for the next cycle
+    decltype(std::chrono::steady_clock::now()) sleep_time = std::chrono::steady_clock::now();
+
     while (!terminate) {
         try {
             wago.fetch_image();
         } catch (const std::exception &e) {
-            std::cerr << "Failed to fetch input image: " << e.what() << std::endl;
+            std::cerr << Print_Time::iso << " ERROR: Failed to fetch input image: " << e.what() << std::endl;
             ret = EX_SOFTWARE;
             break;
         }
@@ -180,12 +205,42 @@ int main(int argc, char **argv) {
         try {
             wago.send_image();
         } catch (const std::exception &e) {
-            std::cerr << "Failed to send output image: " << e.what() << std::endl;
+            std::cerr << Print_Time::iso << " ERROR: Failed to send output image: " << e.what() << std::endl;
             ret = EX_SOFTWARE;
             break;
         }
+
+        if (CYCLE_TIME) {
+            sleep_time = sleep_time + std::chrono::milliseconds(CYCLE_TIME);
+
+            auto n = std::chrono::steady_clock::now();
+
+            if (n > sleep_time) {
+                if (!CYCLE_NOWARN) {
+                    std::cerr << Print_Time::iso << " WARN : Cycle time exceeded by "
+                              << std::chrono::duration_cast<std::chrono::microseconds>(n - sleep_time).count() << "µs"
+                              << std::endl;
+                }
+
+                if (!CYCLE_NOFAIL) {
+                    cycle_fail += 10;
+                    if (cycle_fail > MAX_FAIL) {
+                        std::cerr << Print_Time::iso << " ERROR: cycle time repeatedly exceeded" << std::endl;
+                        ret = EX_TEMPFAIL;
+                        break;
+                    }
+                }
+
+                // set time of last sleep to now, otherwise it will likely fail again in the next cycle
+                sleep_time = n;
+            } else if (cycle_fail) {
+                --cycle_fail;
+            }
+
+            std::this_thread::sleep_until(sleep_time);
+        }
     }
 
-    std::cerr << "Terminating..." << std::endl;
+    std::cerr << Print_Time::iso << " INFO : Terminating..." << std::endl;
     return ret;
 }
